ef1fc802b334adfac56383e4dde5d402750e788d
Chris Jolley <cdjolley@gmail.com>
2017-12-10 19:06:42 -0500

    first commit

---
+++ Portfolio.js
@@ -0,0 +1,59 @@
+function getImages(directory){
+    var dir = "img/Art/";
+    //var folders = ["FINE_ART", "GRAPHIC_DESIGN", "ILLUSTRATION", "PROCESS", "SKETCHBOOKS", "WORK"];
+    if(directory == "ALL"){
+        dir = "ALL";
+    }
+    else{
+        dir = dir + directory;
+    }
+    $.ajax({
+        //This will retrieve the contents of the folder if the folder is configured as 'browsable'
+        url: dir,
+        success: function (data) {
+        $(data).find("a").attr("href", function (i, val) {
+            if( val.match(/\.(jpe?g|png|gif)$/) ) { 
+                $("body").append( "<img src='"+ folder + val +"'>" );
+                } 
+            });
+        }
+    
+    });
+}
+
+function openModal() {
+  document.getElementById('myModal').style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
